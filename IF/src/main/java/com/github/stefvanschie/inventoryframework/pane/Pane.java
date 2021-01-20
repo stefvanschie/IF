4b387edb96b4d8d8cac1a3cd4f8b6cc3be605465
stefvanschie <stefvanschiedev@gmail.com>
2021-01-20 20:20:16 +0100

    Disallow pane lengths and heights of zero
    Such panes will not render and allow for hard to find issues in code. Visibility of panes is better controlled via a pane's internal visibility system. Now an error will be thrown when attempting to create a pane with a length or height of zero.

--- IF/src/main/java/com/github/stefvanschie/inventoryframework/pane/Pane.java
+++ IF/src/main/java/com/github/stefvanschie/inventoryframework/pane/Pane.java
@@ -88,6 +88,10 @@ public abstract class Pane {
      * @param priority the priority of the pane
      */
     protected Pane(int x, int y, int length, int height, @NotNull Priority priority) {
+        if (length == 0 || height == 0) {
+            throw new IllegalArgumentException("Length and height of pane must be greater than zero");
+        }
+
         this.x = x;
         this.y = y;
 
@@ -107,6 +111,10 @@ public abstract class Pane {
      * @param height the height of the pane
      */
     protected Pane(int length, int height) {
+        if (length == 0 || height == 0) {
+            throw new IllegalArgumentException("Length and height of pane must be greater than zero");
+        }
+
         this.length = length;
         this.height = height;
 
